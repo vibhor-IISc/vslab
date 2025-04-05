bc2373e0e09f80a461ad7ce309d58541a324aa62
vibhor-IISc <42720092+vibhor-IISc@users.noreply.github.com>
2025-04-05 13:16:23 +0530

    minor

--- instrument_drivers/FSV13_old.py
+++ instrument_drivers/FSV13_old.py
@@ -27,23 +27,38 @@ class GenerateSetPoints(Parameter):
                               self._numpointsparam())
 
 
+
+
 class psd(ParameterWithSetpoints):
+
+    def _is_complete(self):
+        '''
+        Returns True ONLY when instrument is Ready.
+        To prevent VISA Timeout Error.
+        '''
+        self.root_instrument.write('*ESE 1')
+        # self.write('*ESE 1')
+        try:
+            while not bool(int(self.root_instrument.ask('*OPC; *ESR?'))):
+                sleep(1)
+            return True
+        except KeyboardInterrupt:
+            print('Keyboard intrupption caught.')
+
     def get_raw(self):
         '''
-        Use this method for traces
+        Use this method for time-consuming traces
         which takes more than 4 seconds of time.
         
         Return the trace data in dBm
         
+        
         The function polls the status register to ensure
         when trace is over. This avoid visa TimeOut error
         '''
-        self.root_instrument.write('INIT:CONT OFF')
+        self.root_instrument.write('INIT:CONT OFF;*WAI')
         self.root_instrument.write('INIT')
-        slp = float(self.root_instrument.ask('SWE:TIME?'))*int(self.root_instrument.ask('SWE:COUN?'))
-        sleep(slp + 90)
-        
-        if self.root_instrument.is_complete():
+        if self._is_complete():
             _tr = self.root_instrument.ask('TRAC? TRACE1;*WAI')
         return np.array(_tr.split(',')).astype(np.float)
 
@@ -56,37 +71,46 @@ class psd_fast(ParameterWithSetpoints):
         
         Return the trace data in dBm.
         
-        For longer sweep time, use .trace() function. 
+        For longer sweep time, use get_trace() function. 
         '''
         self.root_instrument.sweep_single()
         _tr = self.root_instrument.ask('TRAC? TRACE1;*WAI')
         return np.array(_tr.split(',')).astype(np.float)
 
 
-class GenerateMarkerArray(Parameter):
+class GenerateMarkerPoint(Parameter):
     '''
-    A parameter that generates the array for the marker frequencies
+    A parameter that generates the list for the marker frequencies
     '''
     def __init__(self, mlist, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._mlist = mlist
 
     def get_raw(self):
-        return self.root_instrument._markers_X()
+        return mlist
 
 
-class MarkerRead(ParameterWithSetpoints):
-    ''' A class to handle the marker read values as parameters'''
+class markervalues(ParameterWithSetpoints):
+    def _get_marker_y(self, marker=1):
+      return float(self.ask('CALC:MARK'+str(marker)+':Y?'))
+
 
     def get_raw(self):
         '''
         Return the corresponding Y values in dBm for an input list
         of frequencies (Hz)
         '''
-        return self.root_instrument._markers_Y()
+        self._markers_place_at(mlist)
+        _Y = []
+        for idx, m in enumerate(mlist):
+            _mY = self._get_marker_y(idx+1)
+            _Y = np.append(_Y, _mY)
+        return _Y
 
 
-class FSV13_2(VisaInstrument):
+
+
+class FSV13(VisaInstrument):
     """
     This is the QCoDeS python driver for Rohde and Schwartz FSV13 signal analyzer.
 
@@ -119,8 +143,8 @@ class FSV13_2(VisaInstrument):
         self.add_parameter(name       = 'center_frequency',
                            unit       = 'Hz',
                            get_parser = float,
-                           set_cmd    = ':SENS:FREQ:CENT {}Hz',
-                           get_cmd    = ':SENS:FREQ:CENT?',
+                           set_cmd    = ':SENS:FREQ:CENT {}Hz;*WAI',
+                           get_cmd    = ':SENS:FREQ:CENT?;*WAI',
                            docstring  = 'Center frequency',
                            vals       = vals.Numbers(10, 13e9)
                            )
@@ -128,8 +152,8 @@ class FSV13_2(VisaInstrument):
         self.add_parameter(name       = 'span',
                            unit       = 'Hz',
                            get_parser = float,
-                           set_cmd    = ':SENS:FREQ:SPAN {}Hz',
-                           get_cmd    = ':SENS:FREQ:SPAN?',
+                           set_cmd    = ':SENS:FREQ:SPAN {}Hz;*WAI',
+                           get_cmd    = ':SENS:FREQ:SPAN?;*WAI',
                            docstring  = 'Span',
                            vals       = vals.Numbers(0, 10e9)
                            )
@@ -137,8 +161,8 @@ class FSV13_2(VisaInstrument):
         self.add_parameter(name       = 'start_frequency',
                            unit       = 'Hz',
                            get_parser = float,
-                           set_cmd    = ':SENS:FREQ:STAR {}Hz',
-                           get_cmd    = ':SENS:FREQ:STAR?',
+                           set_cmd    = ':SENS:FREQ:STAR {}Hz;*WAI',
+                           get_cmd    = ':SENS:FREQ:STAR?;*WAI',
                            docstring  = 'Start frequency',
                            vals       = vals.Numbers(10, 13e9)
                            )
@@ -146,8 +170,8 @@ class FSV13_2(VisaInstrument):
         self.add_parameter(name       = 'stop_frequency',
                            unit       = 'Hz',
                            get_parser = float,
-                           set_cmd    = ':SENS:FREQ:STOP {}Hz',
-                           get_cmd    = ':SENS:FREQ:STOP?',
+                           set_cmd    = ':SENS:FREQ:STOP {}Hz;*WAI',
+                           get_cmd    = ':SENS:FREQ:STOP?;*WAI',
                            docstring  = 'Stop frequency',
                            vals       = vals.Numbers(10, 13e9)
                            )
@@ -155,8 +179,8 @@ class FSV13_2(VisaInstrument):
         self.add_parameter(name       = 'bandwidth',
                            unit       = 'Hz',
                            get_parser = float,
-                           set_cmd    = 'BAND {}Hz',
-                           get_cmd    = 'BAND?',
+                           set_cmd    = 'BAND {}Hz;*WAI',
+                           get_cmd    = 'BAND?;*WAI',
                            docstring  = 'resolution bandwidth',
                            vals       = vals.Enum(*np.append(10**7, np.kron([1, 2, 3, 5], 10 ** np.arange(6))))
                            )
@@ -164,16 +188,16 @@ class FSV13_2(VisaInstrument):
         self.add_parameter(name       = 'video_bandwidth',
                            unit       = 'Hz',
                            get_parser = float,
-                           set_cmd    = 'BAND:VID {}Hz',
-                           get_cmd    = 'BAND:VID?',
+                           set_cmd    = 'BAND:VID {}Hz;*WAI',
+                           get_cmd    = 'BAND:VID?;*WAI',
                            docstring  = 'post-detection filter (VBW)'
                            )
 
         self.add_parameter(name       = 'sweep_points',
                            unit       = '',
                            get_parser = int,
-                           set_cmd    = 'SWE:POIN {}',
-                           get_cmd    = 'SWE:POIN?',
+                           set_cmd    = 'SWE:POIN {};*WAI',
+                           get_cmd    = 'SWE:POIN?;*WAI',
                            docstring  = 'Number of sweep points',
                            vals = vals.Ints(min_value = 101, max_value = 32001)
                            )
@@ -181,8 +205,8 @@ class FSV13_2(VisaInstrument):
         self.add_parameter(name       = 'sweep_count',
                            unit       = '',
                            get_parser = int,
-                           set_cmd    = 'SWE:COUN {}',
-                           get_cmd    = 'SWE:COUN?',
+                           set_cmd    = 'SWE:COUN {};*WAI',
+                           get_cmd    = 'SWE:COUN?;*WAI',
                            docstring  = 'Number of sweep counts',
                            vals = vals.Ints(min_value = 0, max_value = 32767)
                            )
@@ -190,24 +214,24 @@ class FSV13_2(VisaInstrument):
         self.add_parameter(name       = 'sweep_time',
                            unit       = 's',
                            get_parser = float,
-                           set_cmd    = 'SWE:TIME {}s',
-                           get_cmd    = 'SWE:TIME?',
+                           set_cmd    = 'SWE:TIME {}s;*WAI',
+                           get_cmd    = 'SWE:TIME?;*WAI',
                            docstring  = 'Sweep time in seconds',
                            )
 
         self.add_parameter(name       = 'reference_level',
                            unit       = 'dBm',
                            get_parser = int,
-                           set_cmd    = 'DISP:TRAC:Y:RLEV {}dBm',
-                           get_cmd    = 'DISP:TRAC:Y:RLEV?',
+                           set_cmd    = 'DISP:TRAC:Y:RLEV {}dBm;*WAI',
+                           get_cmd    = 'DISP:TRAC:Y:RLEV?;*WAI',
                            docstring  = 'Reference Level in dBm',
                            vals = vals.Ints(min_value = -100, max_value = 30)
                            )
         
         self.add_parameter(name       = 'sweep_average',
                            unit       = None,
-                           set_cmd    = 'DISP:TRAC:MODE {}',
-                           get_cmd    = 'DISP:TRAC:MODE?',
+                           set_cmd    = 'DISP:TRAC:MODE {};*WAI',
+                           get_cmd    = 'DISP:TRAC:MODE?;*WAI',
                            vals = vals.Bool(),
                            val_mapping = create_on_off_val_mapping(on_val= "AVER", off_val= "WRIT"),
                            docstring  = 'Toggle between Average and Clear/Write modes'
@@ -230,7 +254,7 @@ class FSV13_2(VisaInstrument):
                            setpoints  = (self.freq_axis,),
                            parameter_class = psd,
                            vals       = vals.Arrays(shape = (self.sweep_points,)),
-                           docstring  = 'Return PSD msmt. Try "trace_fast" for sweeptime < 4s traces.'
+                           docstring  = 'return power measurement: can be used always. Try "trace_fast" for sweeptime < 4s traces.'
                            )
 
         self.add_parameter(name       = 'trace_fast',
@@ -239,31 +263,14 @@ class FSV13_2(VisaInstrument):
                            setpoints  = (self.freq_axis,),
                            parameter_class = psd_fast,
                            vals       = vals.Arrays(shape = (self.sweep_points,)),
-                           docstring  = 'Return PSD msmt. Use for for fast traces'
+                           docstring  = 'return power measurement: use for for fast traces'
                            )
 
-        self.add_parameter(name       = 'markers_read_X',
-                           unit       = 'Hz',
-                           label      = 'Freq axis',
-                           parameter_class = GenerateMarkerArray,
-                           mlist      = self._markers_X,
-                           vals       = vals.Arrays(shape = (self._markers_count,)),
-                           docstring  = 'return the x-value Array of enabled markers'
-                           )
-
-        self.add_parameter(name       = 'markers_read_Y',
-                           unit       = 'dBm',
-                           label      = 'power',
-                           setpoints  = (self.markers_read_X,),
-                           parameter_class = MarkerRead,
-                           vals       = vals.Arrays(shape = (self._markers_count,)),
-                           docstring  = 'return y-value Array of enabled markers-'
-                           )
 
         
     
-        self.add_function('external_reference', call_cmd='ROSC:SOUR EXT')
-        self.add_function('sweep_cont', call_cmd = 'INIT:CONT ON')
+        self.add_function('external_reference', call_cmd='ROSC:SOUR EXT;*WAI')
+        self.add_function('sweep_cont', call_cmd = 'INIT:CONT ON;*WAI')
         self.add_function('markers_off', call_cmd = 'CALC:MARK:AOFF')
         self.add_function('marker_place_at_max', call_cmd = 'CALC:MARK1:MAX')
         
@@ -273,18 +280,19 @@ class FSV13_2(VisaInstrument):
         
         
     def sweep_single(self):
-        '''  Perform single sweep IMM with *WAI, 
-        use _ = trace() to Trigger longer than 4 seconds sweeps
-        to avoid VISA timeout error.
         '''
-        self.write('INIT:CONT OFF')
+        Perform single sweep IMM with *WAI, 
+        use _ = get_trace() to Trigger longer than 4 seconds sweeps
+        to avoid VISA timeout error.
+        
+        '''
+        self.write('INIT:CONT OFF;*WAI')
         self.write('INIT;*WAI')
     
     def is_complete(self):
         '''
         Returns True ONLY when instrument is Ready.
         To prevent VISA Timeout Error.
-        It blocks further calls from terminal except Keyboard Interupt.
         '''
         self.write('*ESE 1')
         try:
@@ -296,67 +304,34 @@ class FSV13_2(VisaInstrument):
 
 
     def _marker_check_on(self, mbit):
-        ''' Check if nth-marker is enabled/disabled.
-        return bool
+        ''' Return bool if the marker "n" is on/off.
         '''
-        return bool(int(self.ask(f'CALC:MARK{int(mbit)}?')))
-
-    def _marker_get_x(self, mbit):
-        ''' return x-value of a marker at position mbit'''
-        return float(self.ask('CALC:MARK'+str(int(mbit))+':X?'))
-
-    def _marker_set_x(self, freqq, mbit=1):
-        ''' set mbit marker frequency at freqq'''
-        return self.write('CALC:MARK'+str(int(mbit))+f':X {freqq}')
+        return bool(int(sa.ask(f'CALC:MARK{int(mbit)}?')))
 
 
-    def _marker_get_y(self, mbit):
-        ''' return y-value of a marker at position mbit'''
-        return float(self.ask('CALC:MARK'+str(int(mbit))+':Y?'))
-
-    def _markers_count(self):
-        ''' Count the number of enabled markers'''
-        _count = 0
-        for val in np.arange(10):
-            if self._marker_check_on(val+1):
-                _count = _count+1
+    def _markers_read_X(self):
+    '''   return the X values of ALREADY placed markers.
+    MAX_markers = 10'''
+        _ls = []
+        for idx, m in np.arange(10):
+            if self._marker_check_on(idx+1):
+                _x = float(self.ask('CALC:MARK'+str(int(idx+1))+':X?'))
+                _ls = np.append(_ls, _x)
             else:
                 pass
-        return _count
 
-    def markers_add_at(self, mlist):
-        for idx, m in enumerate(mlist):
-            if self._marker_get_x(idx+1) != m:
-                self._marker_set_x(m, idx+1)
+        return _ls
+
+
+    def _markers_read_Y(self):
+    '''   return the Y values of ALREADY placed markers.
+    MAX_markers = 10'''
+        _ls = []
+        for idx, m in np.arange(10):
+            if self._marker_check_on(idx+1):
+                _y = float(self.ask('CALC:MARK'+str(int(idx+1))+':Y?'))
+                _ls = np.append(_ls, _y)
             else:
                 pass
-        pass
 
-
-    def _markers_X(self):
-      _ls = []
-      for idx, m in enumerate(np.arange(10)):
-        if self._marker_check_on(idx+1):
-          _x = self._marker_get_x(idx+1)
-          _ls = np.append(_ls, _x)
-        else:
-          pass
-      return _ls
-
-
-    def _markers_Y(self):
-      _ls = []
-      for idx, m in enumerate(np.arange(10)):
-        if self._marker_check_on(idx+1):
-          _y = self._marker_get_y(idx+1)
-          _ls = np.append(_ls, _y)
-        else:
-          pass
-      return _ls
-
-    def save_settings(self, fname):
-        self.write(f"MMEM:STOR:STAT 1,'{fname}'")
-
-    def load_settings(self, fname):
-        self.write(f"MMEM:LOAD:STAT 1,'C:\\R_S\\Instr\\user\\{fname}'")
-
+        return _ls
