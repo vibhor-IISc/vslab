fc5c8953066d56487d36d5639405e64daec2b2e4
vibhor-IISc <42720092+vibhor-IISc@users.noreply.github.com>
2025-06-05 17:30:27 +0530

    Update BBQ.py

--- _future_/BBQ.py
+++ _future_/BBQ.py
@@ -9,6 +9,12 @@ from scipy.interpolate import UnivariateSpline
 import numpy as np
 import matplotlib.pyplot as plt
 
+from scipy.constants import e as elec, h
+
+LJ = 8*1e-9
+CJ = 5*1e-15
+EC = (elec)**2/2/CJ/h
+
 class Mode:
     def __init__(self, freq, y11):
         '''
@@ -62,12 +68,20 @@ class Mode:
         self.C = Cp
         
         if plot:
-            plt.plot(x,y,'o', label = f'Cp = {Cp:.1e}')
-            plt.plot(x, y11_smooth(x), '--', label = f'Lp = {Lp:.1e}')
+            plt.plot(x,y,'o', label = f'Cp = {Cp:.2e}')
+            plt.plot(x, y11_smooth(x), '--', label = f'Lp = {Lp:.2e}')
             plt.plot(y11_smooth.roots(),[0.0],'*', markersize = 15, label=f'fp = {fp:.3e}')
             plt.legend()
             plt.show()
         return [fp, Cp, Lp]
+    
+    def Kerr_self(self):
+        return -1*(self.L/LJ)*(CJ/self.C)*EC
+    
+    
+    def Kerr_cross(self, mode):
+        return -2*np.sqrt(self.Kerr_self()*mode.Kerr_self())
+    
     
     def detect_zero_crossings(self):
         '''
@@ -81,8 +95,7 @@ class Mode:
 
         '''
         pass
-    
-        
+
         
         
         
